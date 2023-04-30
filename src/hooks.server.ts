0c09caaa1b08260ea0cb84340b6910f14277771d
Maksim Pischulenok <pischule@outlook.com>
2023-04-30 14:40:47 +0300

    Set auth screens theme

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,16 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/core/providers/github";
 import { env } from "$env/dynamic/private";
 import type { Handle } from "@sveltejs/kit";
+import logo from "$lib/assets/logo.svg";
 
 const { AUTH_SECRET, GITHUB_ID, GITHUB_SECRET } = env;
 
 export const handle = SvelteKitAuth({
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
   secret: AUTH_SECRET,
-  trustHost: true
+  trustHost: true,
+  theme: {
+    brandColor: "hsl(195, 85%, 41%)",
+    logo: logo
+  }
 }) satisfies Handle;
